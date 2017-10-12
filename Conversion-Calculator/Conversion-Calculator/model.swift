698c148af63339a175a9d9b7e9aac8a0233efd03
Mandy Rogers <mandyj34@Mandys-MacBook-Pro.local>
2017-10-11 20:29:28 -0500

    pickers are working now with volume and temp data

--- Conversion-Calculator/Conversion-Calculator/model.swift
+++ Conversion-Calculator/Conversion-Calculator/model.swift
@@ -30,6 +30,8 @@ class PopupViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
                 leftPicker = ["0","1","2","3","4","5","6","7","8","9","10"]
                 middlePicker = ["0","1/4","1/3","1/2","2/3","3/4","5/8"]
                 rightPicker = ["Tsp", "Tbsp", "Cup", "Pint", "Quart"]
@@ -49,20 +51,23 @@ class PopupViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
     }
     
     @IBAction func segmentChange(_ sender: Any) {
-        if (segment.selectedSegmentIndex == 0){
-            
+        let index = (sender as AnyObject).selectedSegmentIndex
+        
+        if (index == 0){
             leftPicker = ["0","1","2","3","4","5","6","7","8","9","10"]
             middlePicker = ["0","1/4","1/3","1/2","2/3","3/4","5/8"]
             rightPicker = ["Tsp", "Tbsp", "Cup", "Pint", "Quart"]
-            
-        }else if (segment.selectedSegmentIndex == 1){
-           
+
+        }else if (index == 1){
             leftPicker = ["0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59","60","61","62","63","64","65","66","67","68","69","70","71","72","73","74","75","76","77","78","79","80","81","82","83","84","85","86","87","88","89","90","91","92","93","94","95","96","97","98","99","100"]
             middlePicker = ["0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59","60","61","62","63","64","65","66","67","68","69","70","71","72","73","74","75","76","77","78","79","80","81","82","83","84","85","86","87","88","89","90","91","92","93","94","95","96","97","98","99","100"]
             rightPicker = ["Celsius", "Farenheit"]
-            
+        
+
         }
     }
+    
+    
     //Close popup
     @IBAction func closePopup(_ sender: AnyObject) {
         self.view.removeFromSuperview()
@@ -146,31 +151,32 @@ class PopupViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
         
     }
     
+    @IBAction func segmentControlChange(_ sender: Any) {
+        picker.reloadAllComponents()
+    }
     
     //Returns number of rows for picker, --popup is also rightpicker
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
-                if (component == 0){
+                if (component == 1){
                     return middlePicker.count
-                }else {
+                }else if (component == 0){
                     return leftPicker.count
                 }else {
                     return rightPicker.count
                 }
-        return 0
     }
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 3
     }
     //Returns data for picker, --popup is also rightpicker
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-                if (component == 0){
+                if (component == 1){
                     return self.middlePicker[row]
-                }else{
+                }else if (component == 0){
                     return self.leftPicker[row]
                 }else {
                     return self.rightPicker[row]
                 }
-        return "none"
 }
 
     }
